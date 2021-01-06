596f0ec8dc0b73b1da5b420ec3f871bf28f9ad09
siliun <siliun@nate.com>
2021-01-06 21:28:34 +0900

    지뢰찾기
     - 기능 분리
     - 변수명 수정
     - 랜덤함수 중복 확인 추가

--- src/findBomb/Main.java
+++ src/findBomb/Main.java
@@ -1,28 +1,32 @@
 package findBomb;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Random;
-import java.util.Scanner;
 
+// 지뢰찾기 결과값 생성기능
 public class Main {
-
 	
+	private final static int xAxis = 15; // 박스 가로개수
+	private final static int yAxis = 10; // 박스 새로개수
+	private final static int bombCount = 10; // 지뢰개수
+	private final static int bombVal = -9;	// 지뢰(폭탄)값
+			
 	public static void main(String[] args) {
 		
-		// 숫자 입력
-		int x = 10; // 가로개수
-		int y = 10; // 세로개수
-		int z = 10; // 지뢰개수
-		
 		// 박스 생성
-		int[][] box = createBox(x,y,z);
-		for(int i = 0; i < 10; i++) {
-			for(int j = 0; j < 10; j++ ) {
-				if(box[i][j] > -1) {
-					System.out.print(" "+box[i][j]);
+		Integer[][] mineBox = createMineBox();
+		
+		// 10X10 배열로 결과출력
+		for(int y = yAxis-1 ; y > -1; y--) {
+			for(int x = 0; x < xAxis ; x++ ) {
+//				System.out.print(String.format("[%s,%s]",j,i));
+				if(mineBox[x][y] > -1) {
+					System.out.print(" "+mineBox[x][y]);
 				} else {
-					System.out.print(box[i][j]);
+					System.out.print(mineBox[x][y]);
 				}
-				if(j < 9) {
+				if(x < xAxis-1) {
 					System.out.print(",");
 				} 
 			}
@@ -32,48 +36,108 @@ public class Main {
 	}
 	
 	// 박스 생성 (가로개수, 세로개수, 지뢰개수)
-	public static int[][] createBox(int x, int y, int z){
+	public static Integer[][] createMineBox(){
 		
-		// x, y 배열 생성(0 으로 채우기)
-		int[][] mainBox = new int[x][y];
-		for(int i = 0; i < x; i++) {
-			for(int j = 0; j < y; j++) {
-				mainBox[i][j] = 0;
+		// 가로,세로 값으로 2차 배열 생성(0 으로 채우기)
+		Integer[][] mineBox = makeArray();
+		
+		// 지뢰자표 랜덤 생성
+		mineBox = makeBomb(mineBox);
+		
+		// 지뢰개수 생성
+		mineBox = makeNum(mineBox);		
+				
+		return mineBox;
+	}
+	
+	// 가로,세로 값으로 2차 배열 생성(0 으로 채우기)
+	public static Integer[][] makeArray (){
+		
+		Integer[][] makeArrayBox = new Integer[xAxis][yAxis];	// 리턴값 초기화
+		
+		for(int x = 0; x < xAxis; x++) {
+			for(int y = 0; y < yAxis; y++) {
+				makeArrayBox[x][y] = 0;
 			}
 		}
 		
-		// 지뢰자표 랜덤 생성(지뢰 : -9)
-		Random rd = new Random();
+		return makeArrayBox;
+	}
+	
+	// 지뢰 자표생성
+	public static Integer[][] makeBomb(Integer[][] range){
+
+		Integer[][] makeBombBox = range;	// 리턴값 초기화
+		List<String> bombAxisList = new ArrayList<String>();	// 지뢰좌표 목록 선언
+		Random rd = new Random();	// 랜덤함수 선언
 		
-		for(int i = 0; i < z; i++) {
-			mainBox[rd.nextInt(x)][rd.nextInt(y)] = -9;			
+		// 지뢰개수만큼 좌표목록 생성
+		for(int bombIdx = 0; bombIdx < bombCount; bombIdx++) {
+			// 중복검사를 위한 반복처리
+			while(true){
+				// 좌표생성
+				String axis = rd.nextInt(xAxis)+","+rd.nextInt(yAxis);
+				
+				// 생성된 좌표 추가
+				bombAxisList.add(axis);
+				
+				// 생성된 좌표 중복 검사
+				if(bombAxisList.stream().distinct().count() != bombAxisList.size()) {
+					// 중복된 좌표가 있을경우 마지막 좌표 삭제후 재생성(에러)
+					bombAxisList.remove(bombIdx);
+				} else {
+					// 중복되지 않으면 계속진행(정상)
+					break;
+				}
+				
+			}// while end
+			
+		}	// for end
+						
+		// 생성된 좌료목록에 폭탄값 삽입
+		for(String axis : bombAxisList) {
+			// 가로,세로 분리
+			String[] arrAxis = axis.split(",");
+			// 좌표에 폭탄값 삽입
+			makeBombBox[Integer.parseInt(arrAxis[0])][Integer.parseInt(arrAxis[1])] = bombVal;
 		}
 		
+		return makeBombBox;
 		
-		// 지뢰갯수 구함
-		for(int i = 0; i < x; i++) {
-			for(int j = 0; j < y; j++) {
-			// 한칸당			
-				int p = mainBox[i][j];
-				if (p == -9) {
-					for(int k = i+1; k > i-2 ; k--) {
-						if(k > -1 && k < x) {
-							for(int l = j+1; l > j-2; l--) {
-								if(l > -1 && l < y) {
-									if(mainBox[k][l] > -1) {
-										mainBox[k][l]++;
+	}
+			
+	// 지뢰개수 생성
+	public static Integer[][] makeNum(Integer[][] range){
+
+		Integer[][] makeNumBox = range;	// 리턴값 초기화
+
+		// 박스범위 
+		for(int x = 0; x < xAxis; x++) {
+			for(int y = 0; y < yAxis; y++) {
+
+				int axisVal = makeNumBox[x][y];	// 박스별 값 조회
+				
+				// 지뢰가 존재하는 칸인경우 주위 숫자 입력
+				if (axisVal == bombVal) {
+					// 기준값으로 우좌 좌표 가져오기
+					for(int px = x+1; px > x-2 ; px--) {
+						if(px > -1 && px < xAxis) {	// 박스범위를 벗어나지 않게
+							// 기준값으로 상하 좌표가져오기
+							for(int py = y+1; py > y-2; py--) {
+								if(py > -1 && py < yAxis) {	// 박스범위를 벗어나지 않게
+									if(makeNumBox[px][py] > -1) {
+										makeNumBox[px][py]++;	// 주위 지뢰개수 누적
 									}
 								}
 							}
 						}
-					}
-					
-				} // if end
+					}					
+				} // if(axisVal == bombVal) end
+				
 			} // for(j) end
 		} // for(i) end
 		
-				
-		return mainBox;
+		return makeNumBox;
 	}
 	
 }
